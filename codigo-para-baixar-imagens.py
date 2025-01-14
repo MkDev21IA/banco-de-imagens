5514edaa637abaa02e5c4522eecf003e4950eb8c
MkDev21 <mikhaelpereirasilveira@outlook.com>
2025-01-14 14:56:20 -0300

    Deletado itens não importantes e adicionado pastas jiboia, sucuri assim como o script correto

--- codigo-para-baixar-imagens.py
+++ codigo-para-baixar-imagens.py
@@ -16,37 +16,38 @@ def baixar_imagens(url, pasta, quantidade):
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
+    contador = 0
     # Scroll down to load more images
     body = driver.find_element(By.TAG_NAME, 'body')
-    for _ in range(quantidade // 20):  # Adjust the range as needed
-        body.send_keys(Keys.PAGE_DOWN)
-        time.sleep(1)  # Wait for images to load
+    while contador < quantidade:
+        for _ in range(5):  # Adjust the range as needed
+            body.send_keys(Keys.PAGE_DOWN)
+            time.sleep(1)  # Wait for images to load
 
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        imagens = soup.find_all('img')
+
+        for img in imagens:
+            if contador >= quantidade:
+                break
+            try:
+                img_url = img.get('src')
+                if img_url:
+                    if not img_url.startswith('http'):
+                        img_url = url + img_url
+                    response = requests.get(img_url)
+                    if 'image/jpeg' in response.headers['Content-Type']:  # Check if the image is in JPEG format
+                        img_data = response.content
+                        with open(os.path.join(pasta, f'sucuri_{contador}.jpg'), 'wb') as handler:
+                            handler.write(img_data)
+                        contador += 1
+            except Exception as e:
+                print(f'Erro ao baixar a imagem: {e}')
+
     driver.quit()
 
-    imagens = soup.find_all('img')
-
-    contador = 0
-    for img in imagens:
-        if contador >= quantidade:
-            break
-        try:
-            img_url = img.get('src')
-            if img_url:
-                if not img_url.startswith('http'):
-                    img_url = url + img_url
-                response = requests.get(img_url)
-                if 'image/jpeg' in response.headers['Content-Type']:  # Check if the image is in JPEG format
-                    img_data = response.content
-                    with open(os.path.join(pasta, f'jiboia_{contador}.jpg'), 'wb') as handler:
-                        handler.write(img_data)
-                    contador += 1
-        except Exception as e:
-            print(f'Erro ao baixar a imagem: {e}')
-
-url = 'https://www.google.com/search?tbm=isch&q=Eunectes+urinus'
-pasta = 'sucuris'
+url = 'https://www.google.com/search?q=eunectes+murinus&udm=2'
+pasta = 'sucuri'
 quantidade = 100
 
 baixar_imagens(url, pasta, quantidade)
